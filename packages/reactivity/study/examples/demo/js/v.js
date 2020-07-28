b21bd2621b6e9fd4492b9bfc608b116987c141e5
zwxu01 <zwxu01@wisedu.com>
2020-07-28 17:16:48 +0800

    v

--- packages/reactivity/study/examples/demo/js/v.js
+++ packages/reactivity/study/examples/demo/js/v.js
@@ -1,130 +1,162 @@
 window.V = (function() {
-  const { reactive, effect, readonly } = VueReactivity
+	const {
+		reactive,
+		effect,
+		readonly
+	} = VueReactivity
 
-  const isSymbol = val => typeof val === 'symbol'
+	const isSymbol = val => typeof val === 'symbol'
 
-  const noop = () => {}
+	const noop = () => {}
 
-  const $ = el => {
-    if (typeof el === 'string') {
-      var selected = document.querySelector(el)
-      if (!selected) {
-        return document.createElement('div')
-      }
-      return selected
-    } else {
-      return el
-    }
-  }
+	const callHook = (vm, hook) => {
+		const handlers = vm['options'][hook]
+		if (Array.isArray(handlers)) {
+			handlers.forEach(f => f.call(this))
+		} else if (typeof handlers === 'function') {
+			handlers.call(vm)
+		}
+	}
 
-  function V(options) {
-    this.options = options
-    this.vm = new Vue({})
+	const $ = el => {
+		if (typeof el === 'string') {
+			var selected = document.querySelector(el)
+			if (!selected) {
+				return document.createElement('div')
+			}
+			return selected
+		} else {
+			return el
+		}
+	}
 
-    this.proxyV = null
-    this.$data = this.$methods = this.$props = this._props = {}
-    this._init(options)
-    return this.proxyV
-  }
+	function V(options) {
+		this.options = options
+		this.vm = new Vue({})
 
-  Object.assign(V.prototype, {
-    _init(options) {
-      let value
-      if ((value = options.data)) this._initData(value)
-      if ((value = options.props)) this._initProps(value)
-      this._proxy()
-      if ((value = options.methods))
-        this._initMethods(this.vm._renderProxy, this.proxyV, value)
-    },
-    _initMethods(vm, bindThis, methods) {
-      this.$methods = methods
-      for (let key in methods) {
-        this[key] = vm[key] =
-          methods[key] == null ? noop : methods[key].bind(bindThis)
-      }
-    },
-    _initData(data) {
-      this.$data = reactive(typeof data === 'function' ? data() : data || {})
-    },
-    _initProps(props) {
-      Object.keys(props).map(key => {
-        const value = props[key]
-        if (typeof value === 'object') {
-          this._props[key] = value['default']
-        }
-      })
-      this.$props = readonly(this._props)
-    },
-    _proxy() {
-      const data = this.$data
-      const props = this.$props
+		this.proxyV = null
+		this.$data = this.$methods = this.$props = this._props = {}
+		this._isMounted = false
+		this._init(options)
+		return this.proxyV
+	}
 
-      const has = (target, key) => {
-        return Reflect.has(target, key, target)
-      }
-      const vm = this.vm
+	Object.assign(V.prototype, {
+		_init(options) {
+			let value
+			callHook(this, 'beforeCreate')
+			if ((value = options.data)) this._initData(value)
+			if ((value = options.props)) this._initProps(value)
+			this._proxy()
+			if ((value = options.methods))
+				this._initMethods(this.vm._renderProxy, this.proxyV, value)
+			callHook(this, 'created')
+		},
+		_initMethods(vm, bindThis, methods) {
+			this.$methods = methods
+			for (let key in methods) {
+				this[key] = vm[key] =
+					methods[key] == null ? noop : methods[key].bind(bindThis)
+			}
+		},
+		_initData(data) {
+			this.$data = reactive(typeof data === 'function' ? data() : data || {})
+		},
+		_initProps(props) {
+			Object.keys(props)
+				.map(key => {
+					const value = props[key]
+					if (typeof value === 'object') {
+						this._props[key] = value['default']
+					}
+				})
+			this.$props = readonly(this._props)
+		},
+		_proxy() {
+			const data = this.$data
+			const props = this.$props
 
-      const handlers = {
-        has(target, key) {
-          const result = Reflect.has(target, key)
-          if (
-            !isSymbol(key) &&
-            !target[key] &&
-            (has(data, key) || has(props, key))
-          ) {
-            return true
-          }
-          return result
-        },
-        get(target, key, receiver) {
-          if (!isSymbol(key) && !target[key] && has(data, key)) {
-            return data[key]
-          }
-          if (!isSymbol(key) && !target[key] && has(props, key)) {
-            return props[key]
-          }
-          return Reflect.get(target, key, receiver)
-        },
-        set(target, key, value, receiver) {
-          if (!isSymbol(key) && !target[key] && has(data, key)) {
-            return (data[key] = value)
-          }
-          if (!isSymbol(key) && !target[key] && has(props, key)) {
-            return (data[key] = value)
-          }
-          return Reflect.set(target, key, value, receiver)
-        }
-      }
-      this.vm._renderProxy = new Proxy(this.vm, handlers)
-      this.proxyV = new Proxy(this, handlers)
-    },
-    _mountComponent(el) {
-      const vm = this.vm
-      vm.$el = el
+			const has = (target, key) => {
+				return Reflect.has(target, key, target)
+			}
 
-      const updateComponent = function() {
-        vm._update(vm._render(), false)
-      }
+			const handlers = {
+				has(target, key) {
+					const result = Reflect.has(target, key)
+					if (
+						!isSymbol(key) &&
+						!target[key] &&
+						(has(data, key) || has(props, key))
+					) {
+						return true
+					}
+					return result
+				},
+				get(target, key, receiver) {
+					if (!isSymbol(key) && !target[key] && has(data, key)) {
+						return data[key]
+					}
+					if (!isSymbol(key) && !target[key] && has(props, key)) {
+						return props[key]
+					}
+					return Reflect.get(target, key, receiver)
+				},
+				set(target, key, value, receiver) {
+					if (!isSymbol(key) && !target[key] && has(data, key)) {
+						return (data[key] = value)
+					}
+					if (!isSymbol(key) && !target[key] && has(props, key)) {
+						return (data[key] = value)
+					}
+					return Reflect.set(target, key, value, receiver)
+				}
+			}
+			this.vm._renderProxy = new Proxy(this.vm, handlers)
+			this.proxyV = new Proxy(this, handlers)
+		},
+		_mountComponent(el) {
+			const vm = this.vm
+			vm.$el = el
 
-      effect(updateComponent)
-    },
-    $mount(el) {
-      el = $(el)
-      const options = this.options,
-        vm = this.vm
-      const render = options.render,
-        template = options.template
-      if (!render && template) {
-        const { render, staticRenderFns } = Vue.compile(template)
-        vm.$options.render = render
-        vm.$options.staticRenderFns = staticRenderFns
-      } else {
-        vm.$options.render = render
-      }
-      this._mountComponent(el)
-      return this.proxyV
-    }
-  })
+			const updateComponent = () => {
+				const isMounted = this._isMounted
 
-  return V
+				if (isMounted) {
+					callHook(this, 'beforeUpdate')
+				}
+
+				vm._update(vm._render(), false)
+
+				if (isMounted) {
+					callHook(this, 'updated')
+				}
+			}
+
+			effect(updateComponent)
+		},
+		$mount(el) {
+			el = $(el)
+			const options = this.options,
+				vm = this.vm
+			const render = options.render,
+				template = options.template
+			if (!render && template) {
+				const {
+					render,
+					staticRenderFns
+				} = Vue.compile(template)
+				vm.$options.render = render
+				vm.$options.staticRenderFns = staticRenderFns
+			} else {
+				vm.$options.render = render
+			}
+			callHook(this, 'beforeMount')
+			this._mountComponent(el)
+			callHook(this, 'mounted')
+			this._isMounted = true
+			return this.proxyV
+		}
+	})
+
+	return V
 })()
